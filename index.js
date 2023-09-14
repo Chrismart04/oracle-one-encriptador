92aff63073fb33a6d8e1ee14b38ae5fd60e27d04
Christopher <chrismartinez.2054@gmail.com>
2023-09-13 22:24:19 -0600

    fixed the javascript

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const textArea = documen.querySelector(".text-area");
+const textArea = document.querySelector(".text-area");
 const message = document.querySelector(".resultado");
-const copy = document.querySelector(".btn-copiar");
+const copyButton = document.querySelector(".btn-copiar");
 
 function validateText() {
   let text = document.querySelector(".text-area").value;
-  let validate = text.match(/ˆ[a-z]*$/);
+  let validate = text.match(/^[a-z]*$/);
 
   if (!validate || validate === 0) {
     alert("Solo se permiten letras minusculas y sin acentos");
@@ -13,57 +13,72 @@ function validateText() {
   }
 }
 
-function btnEncriptar(){
-if(!validateText()){
-    const encriptedText = encript(textArea.value)
-    message.value = encriptedText
-    message.style.backgroundImage = "none"
-    textArea.value = ""
-    copy.style.display = "block"
-}
+function btnEncriptar() {
+  if (!validateText()) {
+    const encriptedText = encript(textArea.value);
+    message.value = encriptedText;
+    message.style.backgroundImage = "none";
+    textArea.value = "";
+    copyButton.style.display = "block";
+  }
 }
 
-//encription keys 
+//encription keys
 //the letter "e" is converted to "enter"
 //the letter "i" is converted to "imes"
 //the letter "a" is converted to "ai"
 //the letter "o" is converted to "ober"
 // the letter "u" is converted to "ufat"
 
-function encript(encriptString){
-    let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    encriptString = encriptString.toLowerCase()
+function encript(encriptString) {
+  let matrixCode = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  encriptString = encriptString.toLowerCase();
 
-    for (let i = 0; i < matrixCode.length; i++){
-        if(encriptString.includes(matrixCode[i][0])){
-            encriptString = encriptString.replaceAll(matrixCode[i][0], matrixCode[i][1])
-        }
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (encriptString.includes(matrixCode[i][0])) {
+      encriptString = encriptString.replaceAll(
+        matrixCode[i][0],
+        matrixCode[i][1]
+      );
     }
-    return encriptString
+  }
+  return encriptString;
 }
 
-function btnDecrypt(){
-    const encriptedText = decrypt(textArea.value)
-    message.value = encriptedText
-    textArea.value = ""
+function decrypt(decryptString) {
+  let matrixCode = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  decryptString = decryptString.toLowerCase();
 
-}
-
-function decrypt(){
-    let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    decryptString = decryptString.toLowerCase()
-
-    for (let i = 0; i < matrixCode.length; i++){
-        if(decryptString.includes(matrixCode[i][1])){
-            decryptString = decryptString.replaceAll(matrixCode[i][1], matrixCode[i][0])
-        }
+  for (let i = 0; i < matrixCode.length; i++) {
+    if (decryptString.includes(matrixCode[i][1])) {
+      decryptString = decryptString.replaceAll(
+        matrixCode[i][1],
+        matrixCode[i][0]
+      );
     }
-    return decryptString
+  }
+  return decryptString;
 }
 
-function copy(){
-    message.select();
-    navigator.clipboard.writeText(message.value)
-    message.value = "";
-    alert("Texto Copiado")
+function btnDesencriptar() {
+    const decryptedText = decrypt(message.value);
+    textArea.value = decryptedText; 
+  }
+
+function copyText() {
+  message.select();
+  navigator.clipboard.writeText(message.value);
+  alert("Texto Copiado");
 }
