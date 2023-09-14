c942e9a5efa7ab770e55fea8050d2519e4bed9d2
Christopher <chrismartinez.2054@gmail.com>
2023-09-13 21:57:56 -0600

    finished js

--- index.js
+++ index.js
@@ -16,5 +16,54 @@ function validateText() {
 function btnEncriptar(){
 if(!validateText()){
     const encriptedText = encript(textArea.value)
+    message.value = encriptedText
+    message.style.backgroundImage = "none"
+    textArea.value = ""
+    copy.style.display = "block"
 }
+}
+
+//encription keys 
+//the letter "e" is converted to "enter"
+//the letter "i" is converted to "imes"
+//the letter "a" is converted to "ai"
+//the letter "o" is converted to "ober"
+// the letter "u" is converted to "ufat"
+
+function encript(encriptString){
+    let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    encriptString = encriptString.toLowerCase()
+
+    for (let i = 0; i < matrixCode.length; i++){
+        if(encriptString.includes(matrixCode[i][0])){
+            encriptString = encriptString.replaceAll(matrixCode[i][0], matrixCode[i][1])
+        }
+    }
+    return encriptString
+}
+
+function btnDecrypt(){
+    const encriptedText = decrypt(textArea.value)
+    message.value = encriptedText
+    textArea.value = ""
+
+}
+
+function decrypt(){
+    let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    decryptString = decryptString.toLowerCase()
+
+    for (let i = 0; i < matrixCode.length; i++){
+        if(decryptString.includes(matrixCode[i][1])){
+            decryptString = decryptString.replaceAll(matrixCode[i][1], matrixCode[i][0])
+        }
+    }
+    return decryptString
+}
+
+function copy(){
+    message.select();
+    navigator.clipboard.writeText(message.value)
+    message.value = "";
+    alert("Texto Copiado")
 }
